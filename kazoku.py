aaf547d8174da88a3867361d90e47182049730ed
KUrehauemoto <134258371+KUrehauemoto@users.noreply.github.com>
2023-11-06 15:32:07 +0900

    Add files via upload

---
+++ kazoku.py
@@ -0,0 +1,14 @@
+family = ["Kathuya","Izumi","Kureha","Himari","Yuina"]
+
+print(family[3])
+
+family[3] = "Himarinngo"
+
+if "Shiori" in family:
+    print("Hi Shiori!")
+
+if "Yuina" in family:
+    print("Hi Yuina!!!!!!!!!")
+
+if "Himarinngo" in family:
+    print("Hi I love you")
